2d6e504a79f5e4fbad4c71707a83577ee777a319
Ruan-Portella <ruanmorales29@gmail.br>
2022-12-15 15:41:55 -0300

    Feat: Resolvendo Exercicio 4

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // #Construindo Minhas Funções
 
-// Construindo a função de gerar Core
+// #Construindo Função de DarkMode
 
 function changeMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
 
-const backgroundPalette = () => {
+// Construindo a função de gerar Core
+
+const randomColor = () => {
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
@@ -21,16 +23,25 @@ const newColors = () => {
     const colors = [];
     const palettediv = document.querySelectorAll('.color');
     for (let i = 0; i < palettediv.length; i += 1) {
-      const color = backgroundPalette();
+      const color = randomColor();
       palettediv[0].style.backgroundColor = 'black';
       palettediv[i].style.backgroundColor = color;
       colors.push(color);
     }
   };
 
+// Construindo a função de Trocar as Cores geradas
+
+const buttomEvent = () => {
+  const event = document.querySelector('#button-random-color');
+  event.addEventListener('click', newColors);
+  console.log('teste');
+};
+
 // #Construindo Minhas Chamadas de Funções  
 
 window.onload = () => {
       newColors();
+      buttomEvent();
   };
 
